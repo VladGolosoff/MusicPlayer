366b8a5fcd14060142efd4e1bf144a25cf736de3
VladGolosoff <v.golosoff@gmail.com>
2022-02-15 20:18:22 +0300

    User Interface update

---
+++ MusicPlayer/GradientLayer.swift
@@ -0,0 +1,29 @@
+//
+//  GradientLayer.swift
+//  MusicPlayer
+//
+//  Created by Владислав Голосов on 15.02.2022.
+//
+
+import UIKit
+@IBDesignable
+
+class GradientLayer: UIView {
+    @IBInspectable var topColor: UIColor = .blue
+    @IBInspectable var bottomColor: UIColor = .green
+    
+    var startPointX: CGFloat = 0
+    var startPointY: CGFloat = 0
+    var endPointX: CGFloat = 1
+    var endPointY: CGFloat = 1
+    
+    override func layoutSubviews() {
+        let gradientLayer = CAGradientLayer()
+        gradientLayer.colors = [topColor.cgColor, bottomColor.cgColor]
+        gradientLayer.startPoint = CGPoint(x: startPointX, y: startPointY)
+        gradientLayer.endPoint = CGPoint(x: endPointX, y: endPointY)
+        gradientLayer.frame = self.bounds
+        self.layer.insertSublayer(gradientLayer, at: 0)
+        
+    }
+}
--- MusicPlayer/ViewController.swift
+++ MusicPlayer/ViewController.swift
@@ -6,14 +6,40 @@
 //
 
 import UIKit
+import MediaPlayer
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var albumCover: UIImageView!
+    @IBOutlet weak var trackTitle: UILabel!
+    @IBOutlet weak var playButton: UIButton!
+    @IBOutlet weak var previousButton: UIButton!
+    @IBOutlet weak var nextButton: UIButton!
+    @IBOutlet weak var fadeValueLabel: UILabel!
+    @IBOutlet weak var fadeSlider: UISlider!
+    
+    let trackNames = [
+    "Алла Пугачёва - Позови меня с собой",
+    "The Qemists - Run You"
+    ]
+    
+    func changeTrack() {
+    
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        albumCover.image = UIImage(named: trackNames[1])
+        trackTitle.text = trackNames[1]
+        fadeValueLabel.text = "\(fadeSlider.value)"
+//
+//        let gradientLayer = CAGradientLayer()
+//        gradientLayer.frame = view.bounds
+//        gradientLayer.colors = [
+//            UIColor.systemGreen.cgColor,
+//            UIColor.systemBlue.cgColor
+//        ]
+//        view.layer.addSublayer(gradientLayer)
     }
-
-
+    
+    
 }
 
